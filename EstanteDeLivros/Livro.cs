50a0827a8e1b8e72d5961c3681301c7f638b8abb
AnnyaB <anabordin2008@gmail.com>
2024-04-29 21:22:25 -0300

    Adicionar arquivos de projeto.

---
+++ EstanteDeLivros/Livro.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Livro
+{
+    public string Nome;
+    public string[] Autores;
+    public DateOnly DataLancamento;
+    public string Editora;
+    public string Edicao;
+    public string Ibsm;
+    public int QtdPagina;
+
+    public Livro()
+    {
+    }
+    public void DefinirNome(string n)
+    {
+        this.Nome = n;
+    }
+    public void DefinirQtdAutores(int qtd)
+    {
+        string[] autores = new string[qtd];
+        DefinirAutores(autores, qtd);
+    }
+    public void DefinirAutores(string[] autores, int qtd)
+    {
+        for (int i = 0; i < qtd; i++)
+        {
+            Console.WriteLine($"{i+1}º autor/a:");
+            autores[i] = Console.ReadLine();
+        }
+        this.Autores = autores;
+    }
+    public void DefinirDtLancamento(DateOnly dtl)
+    {
+        this.DataLancamento = dtl;
+    }
+    public void DefinirEditora(string e)
+    {
+        this.Editora = e;
+    }
+    public void DefinirEdicao(string edicao)
+    {
+        this.Edicao = edicao;
+    }
+    public void DefinirIsbm(string i)
+    {
+        this.Ibsm = i;
+    }
+    public void DefinirQtdPagina(int p)
+    {
+        this.QtdPagina = p;
+    }
+    public Livro[] CriarEstante()
+    {
+        Livro[] estante = new Livro[10];
+        return estante;
+    }
+    public void GuardarLivro(Livro livro, Livro[] estante, int index)
+    {
+        estante[index] = livro;
+    }
+
+    public void ListarLivro(Livro[] estante, int index)
+    {
+        if (estante[index].Nome != null)
+        {
+            Console.WriteLine($"Nome do Livro: {estante[index].Nome}");
+            Console.WriteLine("Nome dos autores:");  
+            for (int j = 0; j < Autores.Length; j++)
+                Console.WriteLine($"{Autores[j]}");
+            Console.WriteLine($"Data de lancamento: {estante[index].DataLancamento}");
+            Console.WriteLine($"Editora: {estante[index].Editora}");
+            Console.WriteLine($"Edição: {estante[index].Edicao}");
+            Console.WriteLine($"IBSM: {estante[index].Ibsm}");
+            Console.WriteLine($"Quantidade de páginas: {estante[index].QtdPagina}");
+        }
+    }
+    public void ListarTodosLivros(Livro[] estante, int index)
+    {
+        for (int i = 0; i < index; i++)
+            ListarLivro(estante, i);
+    }
+}
+
---
+++ EstanteDeLivros/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+faça um programa de estante de livro, onde a estante é um vetor de até 10 livros. No final mostra a estante inteira ou o livro na posição escolhida;
+
+titulo;
+autor ou autores;
+data lançamento;
+editora;
+edição;
+isbm;
+qtd pagina;
+*/
+
+int index = 0;
+Livro livro = new Livro();
+Livro[] estante = livro.CriarEstante();
+int opcao = 0;
+Console.WriteLine("-Guarde seus livros!!-");
+do
+{
+    Console.WriteLine($"{index+1} livro:");
+    Console.WriteLine("Digite o nome do livro:");
+    livro.DefinirNome(Console.ReadLine());
+
+    Console.WriteLine("Digite a quantidade de autores do livro:");
+    livro.DefinirQtdAutores(int.Parse(Console.ReadLine()));
+
+    Console.WriteLine("Digite a data de lançamento do livro:");
+    livro.DefinirDtLancamento(DateOnly.Parse(Console.ReadLine()));
+    
+    Console.WriteLine("Digite a editora do livro:");
+    livro.DefinirEdicao(Console.ReadLine());
+
+    Console.WriteLine("Digite o nome da editora livro:");
+    livro.DefinirEditora(Console.ReadLine());
+
+    Console.WriteLine("Digite ISBM do livro:");
+    livro.DefinirIsbm(Console.ReadLine());
+
+    Console.WriteLine("Digite a quantidade de páginas do livro:");
+    livro.DefinirQtdPagina(int.Parse(Console.ReadLine()));
+
+    livro.GuardarLivro(livro, estante, index);
+    index++;
+
+    Console.WriteLine("Digite 0 para parar ou qualquer outro número para continuar.");
+    opcao = int.Parse(Console.ReadLine());
+
+} while (opcao != 0 || index > 10);
+
+do
+{
+    Console.WriteLine("Deseja mostrar livro?\n digite a posição desejada, ou digite 0 para mostrar todos ou digite 11 para sair.");
+    opcao = int.Parse(Console.ReadLine());
+    if (opcao == 0)
+        livro.ListarTodosLivros(estante, index);
+    else if (opcao < 11)
+        livro.ListarLivro(estante, opcao - 1);
+} while (opcao != 11);
